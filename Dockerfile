3917b252bcc2a5f17b034acf0c420d4f40ea6f05
yuta-2001 <yu0210ta.sug@gmail.com>
2023-10-10 01:10:21 +0900

    connect with line

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,7 @@ FROM python:3.8
 
 WORKDIR /usr/src/app
 
-ENV FLASK_APP=app
-
 COPY /app/requirements.txt ./
 
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip && \
+    pip install -r requirements.txt
--- app/app.py
+++ app/app.py
@@ -1,7 +1,42 @@
-from flask import Flask
+import os
+
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.config['CHANNEL_SECRET'] = os.getenv('CHANNEL_SECRET')
+app.config['CHANNEL_ACCESS_TOKEN'] = os.getenv('CHANNEL_ACCESS_TOKEN')
+app.config['GOOGLE_MAP_API_KEY'] = os.getenv('GOOGLE_MAP_API_KEY')
+
+handler = WebhookHandler(app.config['CHANNEL_SECRET'])
+line_bot_api = LineBotApi(app.config['CHANNEL_ACCESS_TOKEN'])
+
+## LINE Bot API Webhook
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+## test page
 @app.route("/")
 def index():
     return "index page"
