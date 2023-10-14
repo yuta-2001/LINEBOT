66c5089d3afb8649cd2399141ef7b6b7c2f7a63f
yuta-2001 <yu0210ta.sug@gmail.com>
2023-10-14 18:47:17 +0900

    reply to user

--- api/routers/line.py
+++ api/routers/line.py
@@ -1,16 +1,30 @@
-from fastapi import APIRouter, Header, Request
-# from modules.line import getHandler
+from fastapi import (
+    APIRouter,
+    Header, 
+    Request
+)
 import os
 from dotenv import load_dotenv
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
 from starlette.exceptions import HTTPException
 
 load_dotenv()
 router = APIRouter()
 
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
-line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
+configuration = Configuration(access_token=os.environ.get('CHANNEL_ACCESS_TOKEN'))
 
 @router.post(
     '/api/callback',
@@ -27,3 +41,14 @@ async def callback(request: Request, x_line_signature=Header(None)):
         raise HTTPException(status_code=400, detail="InvalidSignatureError")
 
     return "OK"
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event: MessageEvent):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
